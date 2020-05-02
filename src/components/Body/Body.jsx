b14314675c606943ff1bc58f5372683953606d24
Raymond Akalonu <raymondosy1@gmail.com>
2020-05-02 16:16:36 +0100

    feat(navbar): design navigation bar, setup thailwind & update dependencies

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Body = () => (
+  <div>
+    <section>
+      <h1>I am the body!</h1>
+    </section>
+  </div>
+);
+
+export default Body;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Body from "../components/Body/Body"
+
+const Homepage = () => (
+  <div class="flex flex-col">
+    <Navbar/>
+    <Body/>
+  </div>
+);
+
+export default Homepage;
