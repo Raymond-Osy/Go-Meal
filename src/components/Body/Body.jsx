6b5134509a180ba7bd9a560ad7d3b3c5de016502
Raymond Akalonu <raymondosy1@gmail.com>
2020-05-04 23:59:43 +0100

    feat(body): add body section

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import mainImg from "../../assets/images/food7.jpg";
 
 const Body = () => (
   <div>
+    <section class="flex items-center my-20 px-8">
+        <div class="flex-1 py-12 px-2">
+            <h1 class="text-3xl font-extrabold">Order delicious meal on the go!</h1>
+            <p class="text-xl font-hairline my-12">We prepare your favorite dishes and deliver it at your door step</p>
+            <a class="text-center rounded-full py-4 px-12 bg-green-600 hover:bg-green-700 text-white" href="#">I'm Hungry</a>
+        </div>
+        <div class="flex-1 hidden md:block">
+            <img src={mainImg} alt="Image of rice delicacy"/>
+        </div>
+    </section>
     <section>
-      <h1>I am the body!</h1>
+      <div>
+        <svg src=""></svg>
+        <p>These are a brief decription</p>
+      </div>
     </section>
   </div>
 );
