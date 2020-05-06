8517bde2c2b234dd3347c598811b068a5bbbac45
Raymond Akalonu <raymondosy1@gmail.com>
2020-05-06 06:10:19 +0100

    feat(desc): add easy steps to body description

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,22 +1,54 @@
 import React from "react";
 import mainImg from "../../assets/images/food7.jpg";
+import orderMeal from "../../assets/svg/order.svg";
+import foodRest from "../../assets/svg/food-rest1.svg";
+import food from "../../assets/svg/food.svg";
 
 const Body = () => (
   <div>
-    <section class="flex items-center my-20 px-8">
-        <div class="flex-1 py-12 px-2">
-            <h1 class="text-3xl font-extrabold">Order delicious meal on the go!</h1>
-            <p class="text-xl font-hairline my-12">We prepare your favorite dishes and deliver it at your door step</p>
-            <a class="text-center rounded-full py-4 px-12 bg-green-600 hover:bg-green-700 text-white" href="#">I'm Hungry</a>
+    <section className="flex items-center my-20 px-8">
+        <div className="flex-1 py-2 px-4 mt-24">
+            <h1 className="text-3xl font-extrabold uppercase leading-tight">Order delicious meal on the go!</h1>
+            <p className="text-gray-700 text-xl font-hairline mt-5 mb-10">We prepare your favorite dishes and deliver it at your door step</p>
+            <a className="text-center rounded-full py-4 px-12 bg-green-600 hover:bg-green-700 text-white" href="#">I'm Hungry</a>
         </div>
-        <div class="flex-1 hidden md:block">
+        <div className="flex-1 hidden md:block">
             <img src={mainImg} alt="Image of rice delicacy"/>
         </div>
     </section>
-    <section>
-      <div>
-        <svg src=""></svg>
-        <p>These are a brief decription</p>
+
+    <section className="bg-gray-100">
+      <div className="flex justify-center">
+        <h1 className="text-gray-900 text-3xl font-extrabold leading-tight mt-5">How it works</h1>
+      </div>
+      <div className="flex flex-col md:flex-row  px-8 items-center justify-center">
+        <div className="bg-white rounded overflow-hidden max-w-xs shadow-2xl text-center items-center p-2 m-6">
+          <img className="w-3/12 m-auto mt-3" src={orderMeal} alt="order meal on the go"/>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Getting your favorite dish has never been more easier. Order that dream meal with just the click of your fingers, no stress.
+            </p>
+          </div>
+        </div>
+        <div className="bg-white rounded overflow-hidden max-w-xs shadow-2xl text-center items-center p-2 m-6">
+          <img className="w-3/12 m-auto mt-3" src={foodRest} alt="order meal on the go"/>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Getting your favorite dish has never been more easier. Order that dream meal with just the click of your fingers, no stress.
+            </p>
+          </div>
+        </div>
+        <div className="bg-white rounded overflow-hidden max-w-xs shadow-2xl text-center items-center p-2 m-6">
+          <img className="w-3/12 m-auto mt-3" src={food} alt="order meal on the go"/>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Getting your favorite dish has never been more easier. Order that dream meal with just the click of your fingers, no stress.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 
 const Navbar = () => (
-  <nav class="flex fixed w-full top-0 items-center justify-between flex-wrap p-6 shadow-lg bg-white">
-    <header class="flex items-center flex-shrink-0 text-red-500 mr-6">
-      <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-      <span class="font-semibold text-xl tracking-tight">Go Meal</span>
+  <nav className="flex fixed w-full top-0 items-center justify-between flex-wrap p-6 shadow-lg bg-white">
+    <header className="flex items-center flex-shrink-0 text-red-500 mr-6">
+      <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+      <span className="font-semibold text-xl tracking-tight">Go Meal</span>
     </header>
 
 {/*  Harmbuger for screens smaller screens */}
-    <div class="block lg:hidden">
-      <button class="flex items-center px-3 py-2 border rounded text-black-200 border-black-400 hover:text-red-500 hover:border-red-500">
-        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    <div className="block lg:hidden">
+      <button className="flex items-center px-3 py-2 border rounded text-black-200 border-black-400 hover:text-red-500 hover:border-red-500">
+        <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
       </button>
     </div>
 
-    <div class="w-full block flex-grow lg:justify-end lg:flex lg:items-center lg:w-auto">
-      <div class="text-base">
-        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:text-red-500 mr-8">
+    <div className="w-full block flex-grow lg:justify-end lg:flex lg:items-center lg:w-auto">
+      <div className="text-base">
+        <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:text-red-500 mr-8">
           Sign in
         </a>
 
-        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:text-red-500 mr-8">
+        <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:text-red-500 mr-8">
           Sign up
         </a>
       </div>
